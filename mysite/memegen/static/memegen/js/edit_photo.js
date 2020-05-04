12a487a973cae7ded88a82e366b7dd70ad32f2f5
peterlc-0881 <peterlc@umich.edu>
2020-05-03 20:40:41 -0400

    Fixed some bugs re: obj placement on webpage

--- mysite/memegen/static/memegen/js/edit_photo.js
+++ mysite/memegen/static/memegen/js/edit_photo.js
@@ -4,19 +4,23 @@ change_photo = function() {
 	var canvas = document.getElementById("img-canvas");
 	var src_img = document.getElementById("src-img");
 	$(src_img).hide();
+	canvas.hidden = false;
 
 	console.log($(src_img).height() + ", " + $(src_img).width());
 
-	$(canvas).height($(src_img).height() + 100)
-		.width($(src_img).width());
-
+	/*
+	$(canvas).height($(src_img).height() + 20)
+		.width($(src_img).width() + 20);
+	*/
 	var ctx	= canvas.getContext("2d");
 
 	ctx.drawImage(src_img, 10, 10);
 
 	ctx.lineWidth=1;
-	ctx.fillStyle="#CC00FF";
+	ctx.fillStyle="#000000";
 	ctx.lineStyle="#ffff00";
-	ctx.font="18px sans-serif";
-	ctx.fillText($("#form_caption").val(), 50, 50);
+	ctx.font="36px sans-serif";
+	ctx.fillText($("#form_caption").val(), 10 + $(src_img).height() / 2, 10 + $(src_img).width() / 2);
+
+	$(canvas).show();
 }
