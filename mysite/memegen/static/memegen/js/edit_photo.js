5bc02871cbb961b54139876adbb5a8a69e452c14
peterlc-0881 <peterlc@umich.edu>
2020-05-04 13:16:03 -0400

    Changed hhow the browser displayed edits to photo

--- mysite/memegen/static/memegen/js/edit_photo.js
+++ mysite/memegen/static/memegen/js/edit_photo.js
@@ -1,26 +1,36 @@
+window.onload = function () {
+	console.log("hi!");
+	var canvas = document.getElementById("img-canvas");
+	var src_img = document.getElementById("src-img");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.drawImage(src_img, 0, 0);
+};
+
 change_photo = function() {
-	console.log("help!");
+	console.log("hi!");
 
 	var canvas = document.getElementById("img-canvas");
 	var src_img = document.getElementById("src-img");
+
+	/*
 	$(src_img).hide();
 	canvas.hidden = false;
 
 	console.log($(src_img).height() + ", " + $(src_img).width());
 
-	/*
 	$(canvas).height($(src_img).height() + 20)
 		.width($(src_img).width() + 20);
 	*/
 	var ctx	= canvas.getContext("2d");
 
-	ctx.drawImage(src_img, 10, 10);
+	ctx.drawImage(src_img, 0, 0);
 
 	ctx.lineWidth=1;
 	ctx.fillStyle="#000000";
 	ctx.lineStyle="#ffff00";
 	ctx.font="36px sans-serif";
-	ctx.fillText($("#form_caption").val(), 10 + $(src_img).height() / 2, 10 + $(src_img).width() / 2);
+	ctx.fillText($("#form_caption").val(), 50, 50);
 
-	$(canvas).show();
-}
+	//$(canvas).show();
+};
